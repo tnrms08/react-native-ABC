7758b0886b5442f0e0e54f9e5e6ba4ac1bfb583c
tnrms08 <wjddlfla661@naver.com>
2022-05-18 22:40:27 +0900

    commit9

--- screens/WriteHistoryTab/IncomeTab.js
+++ screens/WriteHistoryTab/IncomeTab.js
@@ -1,28 +1,111 @@
 import { useNavigation } from '@react-navigation/core';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown';
+
+import DateTimePicker from 'react-native-modal-datetime-picker';
 
 const IncomeTab = () => {
 
     const navigation = useNavigation()
 
+    const bank=["농협","하나","카카오뱅크","신한","우리","현금"]
+    const item=["용돈","장학금","이자","더치페이","기타"]
+
+    const [money,setMoney]=useState('')
+;
+
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+    const showDatePicker = () => {
+        setDatePickerVisibility(true);
+    };
+
+    const hideDatePicker = () => {
+        setDatePickerVisibility(false);
+    };
+
+    const handleConfirm = (date) => {
+        console.warn("A date has been picked: ", date);
+        hideDatePicker();
+    };
+
     return (
       <View style={styles.container}>
-        <View>
-          <Text>날짜</Text>
+
+        <View style={{flexDirection: 'row', marginBottom:20, marginTop:50}}>
+          <Text style={{paddingRight: 25, fontSize:20, paddingTop:10}}>날짜</Text>
+          <View style={{paddingHorizontal:65}}>
+            <Button title="날짜 선택" onPress={showDatePicker} />
+            <DateTimePicker
+              isVisible={isDatePickerVisible}
+              mode="date"
+              onConfirm={handleConfirm}
+              onCancel={hideDatePicker}
+            />
+          </View>
         </View>
-        <View>
-          <Text>자산</Text>
+          
+        <View style={{flexDirection: 'row', marginBottom:20}}>
+          <Text style={{paddingRight: 25, fontSize:20, paddingTop:10}}>자산</Text>
+          <View>
+            <SelectDropdown
+              data={bank}
+              onSelect={(selectedItem, index) => {
+                console.log(selectedItem, index)
+              }}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                return selectedItem
+              }}
+              rowTextForSelection={(item, index) => {
+                return item
+              }}
+              defaultButtonText="선택"
+            />
+          </View>
         </View>
-        <View>
-          <Text>분류</Text> 
+
+        <View style={{flexDirection: 'row', marginBottom:20}}>
+          <Text style={{paddingRight: 25, fontSize:20, paddingTop:10}}>분류</Text>
+          <View>
+            <SelectDropdown
+              data={item}
+              onSelect={(selectedItem, index) => {
+                console.log(selectedItem, index)
+              }}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                return selectedItem
+              }}
+              rowTextForSelection={(item, index) => {
+                return item
+              }}
+              defaultButtonText="선택"
+            />
+          </View>
         </View>
-        <View>
-          <Text>금액</Text>
+
+        <View style={{flexDirection: 'row', marginBottom:20}}>
+          <Text style={{fontSize:20, paddingTop:10, paddingRight:80, alignItems:'center', justifyContent: "center"}}>금액</Text>
+          <View>
+            <TextInput
+              placeholder='금액 입력'
+              onChangeText={text=>setMoney(text)}
+              style={{fontSize:20, paddingRight:70}}
+            />
+          </View>
         </View>
-        <View>
-          <Text>내용</Text>
+
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{fontSize:20, paddingTop:10, paddingRight:80, alignItems:'center', justifyContent: "center"}}>내용</Text>
+          <View>
+            <TextInput
+              placeholder='내용 입력'
+              onChangeText={text=>setMoney(text)}
+              style={{fontSize:20, paddingRight:70}}
+            />
+          </View>
         </View>
+
         <View style={styles.container}>
           <TouchableOpacity style={[styles.button, styles.buttonOutline]}>
             <Text style={styles.buttonText}>저장하기</Text>
@@ -34,7 +117,7 @@ const IncomeTab = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     justifyContent: 'center',
     alignItems: 'center'
   },
